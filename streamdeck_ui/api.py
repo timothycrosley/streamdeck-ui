0a7840ef1f3c0916420b37f9ba3c2aaef7dde095
Timothy Crosley <timothy.crosley@gmail.com>
2019-09-28 22:56:28 -0700

    Start adding state; toggle selected stream deck button

--- streamdeck_ui/api.py
+++ streamdeck_ui/api.py
@@ -3,6 +3,8 @@ from typing import Dict, List, Tuple, Union
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.Devices import StreamDeck
 
+state: Dict[str, Dict[int, Dict[str, str]]] = {}
+
 
 def decks() -> Dict[str, Dict[str, Union[str, Tuple[int, int]]]]:
     """Returns all decks we know about"""
@@ -12,3 +14,15 @@ def decks() -> Dict[str, Dict[str, Union[str, Tuple[int, int]]]]:
         deck.id().decode(): {"type": deck.deck_type(), "layout": deck.key_layout()}
         for deck in streamdecks
     }
+
+
+def set_button_text(deck_id: str, button: int, text: str) -> None:
+    """Set the text associated with a button"""
+    state.setdefault(deck_id, {}).setdefault(button, {})["text"] = text
+
+
+def set_button_icon(deck_id: str, button: int, icon: str) -> None:
+    """Sets the icon associated with a button"""
+    state.setdefault(deck_id, {}).setdefault(button, {})["icon"] = icon
+
+
--- streamdeck_ui/gui.py
+++ streamdeck_ui/gui.py
@@ -3,11 +3,23 @@ import sys
 
 from PySide2 import QtWidgets
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
+from PySide2.QtWidgets import QApplication, QSizePolicy
 
 from streamdeck_ui import api
 
 STREAMDECK_TEMPLATE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "main.ui")
+
+
+selected_button = None
+
+
+def button_clicked(clicked_button, buttons):
+    selected_button = clicked_button
+    for button in buttons:
+        if button == clicked_button:
+            continue
+
+        button.setChecked(False)
 
 
 def start():
@@ -18,9 +30,34 @@ def start():
 
     for deck_id, deck in api.decks().items():
         window.device_list.addItem(f"{deck['type']} - {deck_id}", userData=deck_id)
+
+        tab = window.cards.currentWidget()
+        row_layout = QtWidgets.QVBoxLayout()
+        tab.children()[0].addLayout(row_layout, 0, 0)
+
+        buttons = []
+        index = 0
         for row in range(deck["layout"][0]):
+            column_layout = QtWidgets.QHBoxLayout()
+            row_layout.addLayout(column_layout)
+
             for column in range(deck["layout"][1]):
-                window.cards.currentWidget().children()[0].addWidget(QtWidgets.QPushButton())
+                index += 1
+                button = QtWidgets.QPushButton()
+                button.setCheckable(True)
+                button.index = index
+                button.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+                button.setStyleSheet("""QPushButton{background-color:black;}
+                                        QPushButton:checked{background-color:darkGray;}
+                                        QPushButton:focus{border:none; }
+                                     """)
+                buttons.append(button)
+                column_layout.addWidget(button)
+
+        for button in buttons:
+            button.clicked.connect(lambda button=button, buttons=buttons: button_clicked(button, buttons))
+
+        buttons[0].click()
 
 
     return app.exec_()
