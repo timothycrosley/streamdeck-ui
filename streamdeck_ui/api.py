aa6ccbe2b2188e66dba1143311b827e9dafa6b9e
Timothy Crosley <timothy.crosley@gmail.com>
2019-09-30 23:22:13 -0700

    Initial working api to set text and icons for buttons

--- streamdeck_ui/api.py
+++ streamdeck_ui/api.py
@@ -1,26 +1,88 @@
+import os
+import threading
 from typing import Dict, List, Tuple, Union
 
+from PIL import Image, ImageDraw, ImageFont
 from StreamDeck.DeviceManager import DeviceManager
-from StreamDeck.Devices import StreamDeck
+from StreamDeck.Devices.StreamDeck import StreamDeck
+from StreamDeck.ImageHelpers import PILHelper
+
+FONTS_PATH = os.path.join(os.path.dirname(__file__), "fonts")
+DEFAULT_FONT = os.path.join("roboto", "Roboto-Regular.ttf")
 
 state: Dict[str, Dict[int, Dict[str, str]]] = {}
+decks: Dict[str, StreamDeck] = {deck.id().decode(): deck for deck in DeviceManager().enumerate()}
 
 
-def decks() -> Dict[str, Dict[str, Union[str, Tuple[int, int]]]]:
-    """Returns all decks we know about"""
-    streamdecks: List[StreamDeck] = DeviceManager().enumerate()
-    [deck.open() for deck in streamdecks]
+def open_decks() -> Dict[str, Dict[str, Union[str, Tuple[int, int]]]]:
+    """Opens and then returns all known stream deck devices"""
+    [deck.open() for deck in decks.values()]
     return {
-        deck.id().decode(): {"type": deck.deck_type(), "layout": deck.key_layout()}
-        for deck in streamdecks
+        deck_id: {"type": deck.deck_type(), "layout": deck.key_layout()}
+        for deck_id, deck in decks.items()
     }
 
 
 def set_button_text(deck_id: str, button: int, text: str) -> None:
     """Set the text associated with a button"""
     state.setdefault(deck_id, {}).setdefault(button, {})["text"] = text
+    render()
+
+
+def get_button_text(deck_id: str, button: int) -> str:
+    """Returns the text set for the specified button"""
+    return state.get(deck_id, {}).get(button, {}).get("text", "")
 
 
 def set_button_icon(deck_id: str, button: int, icon: str) -> None:
     """Sets the icon associated with a button"""
     state.setdefault(deck_id, {}).setdefault(button, {})["icon"] = icon
+
+
+def get_button_icon(deck_id: str, button: int) -> str:
+    """Returns the icon set for a particular button"""
+    return state.get(deck_id, {}).get(button, {}).get("icon", "")
+
+
+def set_button_command(deck_id: str, button: int, command: str) -> None:
+    """Sets the command associated with the button"""
+    state.setdefault(deck_id, {}).setdefault(button, {})["command"] = command
+
+
+def get_button_command(deck_id: str, button: int) -> str:
+    """Returns the command set for the specified button"""
+    return state.get(deck_id, {}).get(button, {}).get("command", "")
+
+
+def render() -> None:
+    """renders all decks"""
+    for deck_id, buttons in state.items():
+        for button_id, button_settings in buttons.items():
+            if "text" in button_settings or "icon" in button_settings:
+                image = render_key_image(decks[deck_id], **button_settings)
+                decks[deck_id].set_key_image(button_id, image)
+
+
+def render_key_image(deck, icon: str = "", text: str = "", font: str = DEFAULT_FONT):
+    # Create new key image of the correct dimensions, black background
+    image = PILHelper.create_image(deck)
+    draw = ImageDraw.Draw(image)
+
+    # Add image overlay, rescaling the image asset if it is too large to fit
+    # the requested dimensions via a high quality Lanczos scaling algorithm
+    if icon:
+        rgba_icon = Image.open(icon).convert("RGBA")
+    else:
+        rgba_icon = Image.new("RGBA", (300, 300))
+    rgba_icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
+    icon_pos = ((image.width - rgba_icon.width) // 2, 0)
+    image.paste(rgba_icon, icon_pos, rgba_icon)
+
+    # Load a custom TrueType font and use it to overlay the key index, draw key
+    # label onto the image
+    true_font = ImageFont.truetype(os.path.join(FONTS_PATH, font), 14)
+    label_w, label_h = draw.textsize(text, font=true_font)
+    label_pos = ((image.width - label_w) // 2, image.height - 20)
+    draw.text(label_pos, text=text, font=true_font, fill="white")
+
+    return PILHelper.to_native_format(deck, image)
--- streamdeck_ui/gui.py
+++ streamdeck_ui/gui.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from functools import partial
 
 from PySide2 import QtWidgets
 from PySide2.QtUiTools import QUiLoader
@@ -9,11 +10,21 @@ from streamdeck_ui import api
 
 STREAMDECK_TEMPLATE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "main.ui")
 
-
 selected_button = None
 
 
-def button_clicked(clicked_button, buttons):
+def update_button_text(window, text):
+    deck_id = window.device_list.itemData(0)
+    api.set_button_text(deck_id, selected_button.index, text)
+
+
+def update_button_command(window, command):
+    deck_id = window.device_list.itemData(0)
+    api.set_button_command(deck_id, selected_button.index, command)
+
+
+def button_clicked(window, clicked_button, buttons):
+    global selected_button
     selected_button = clicked_button
     for button in buttons:
         if button == clicked_button:
@@ -21,14 +32,20 @@ def button_clicked(clicked_button, buttons):
 
         button.setChecked(False)
 
+    deck_id = window.device_list.itemData(0)
+    button_id = selected_button.index
+    window.text.setText(api.get_button_text(deck_id, button_id))
+    window.command.setText(api.get_button_command(deck_id, button_id))
+
 
 def start():
     app = QApplication(sys.argv)
-
     window = QUiLoader().load(STREAMDECK_TEMPLATE)
     window.show()
 
-    for deck_id, deck in api.decks().items():
+    window.text.textChanged.connect(partial(update_button_text, window))
+    window.command.textChanged.connect(partial(update_button_command, window))
+    for deck_id, deck in api.open_decks().items():
         window.device_list.addItem(f"{deck['type']} - {deck_id}", userData=deck_id)
 
         tab = window.cards.currentWidget()
@@ -37,12 +54,11 @@ def start():
 
         buttons = []
         index = 0
-        for row in range(deck["layout"][0]):
+        for _row in range(deck["layout"][0]):
             column_layout = QtWidgets.QHBoxLayout()
             row_layout.addLayout(column_layout)
 
-            for column in range(deck["layout"][1]):
-                index += 1
+            for _column in range(deck["layout"][1]):
                 button = QtWidgets.QPushButton()
                 button.setCheckable(True)
                 button.index = index
@@ -55,10 +71,11 @@ def start():
                 )
                 buttons.append(button)
                 column_layout.addWidget(button)
+                index += 1
 
         for button in buttons:
             button.clicked.connect(
-                lambda button=button, buttons=buttons: button_clicked(button, buttons)
+                lambda button=button, buttons=buttons: button_clicked(window, button, buttons)
             )
 
         buttons[0].click()
