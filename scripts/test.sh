2ca30f841307a09c059d6d5ed008b487f1a849c2
Timothy Crosley <timothy.crosley@gmail.com>
2019-10-08 03:04:09 -0700

    Report coverage as XML

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
 ./scripts/lint.sh
-poetry run pytest -s --cov=streamdeck_ui/ --cov=tests --cov-report=term-missing ${@} --cov-report html
+poetry run pytest -s --cov=streamdeck_ui/ --cov=tests --cov-report=term-missing ${@} --cov-report xml
