7072ccb191abf2fe50274758fbedf91e00312ef8
Timothy Crosley <timothy.crosley@gmail.com>
2019-12-01 00:01:37 -0800

    Add test file

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,4 @@
 set -euxo pipefail
 
 ./scripts/lint.sh
-<<<<<<<
-=======
 poetry run pytest -s --cov=streamdeck_ui/ --cov=tests --cov-report=term-missing ${@-} --cov-report html
->>>>>>>
-poetry run pytest -s --cov=streamdeck_ui/ --cov=tests --cov-report=term-missing ${@} --cov-report xml
