2b5213783d5154280c3679f6d02377669c7a6e23
techpoet <daniel.krefft@tum.de>
2020-11-14 16:48:47 +0100

    remove 'plugdev' group in fedora install
    'plugdev' is an ubuntu-specific group and does
    not exist on fedora. Thus, this patch removes the group and just
    keeps the mode.

--- scripts/fedora_install.sh
+++ scripts/fedora_install.sh
@@ -2,13 +2,12 @@
 echo "Installing libraries"
 sudo dnf install python3-devel libusb-devel libusbx-devel libudev-devel systemd-devel
 echo "Adding udev rules and reloading"
-sudo usermod -a -G plugdev `whoami`
 
 sudo tee /etc/udev/rules.d/99-streamdeck.rules << EOF
-SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", MODE:="666", GROUP="plugdev"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", MODE:="666", GROUP="plugdev"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006c", MODE:="666", GROUP="plugdev"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", MODE:="666", GROUP="plugdev"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0060", MODE:="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="0063", MODE:="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006c", MODE:="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="0fd9", ATTRS{idProduct}=="006d", MODE:="0666"
 EOF
 
 sudo udevadm control --reload-rules
