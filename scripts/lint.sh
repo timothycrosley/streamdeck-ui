4f758777a5422bdf6db7a6c8cfaac2b24ebbc9d2
Pieter Venter <dodgyrabbit@hotmail.com>
2021-03-24 13:48:12 -0400

    Update pillow to fix security lint
    Exclude auto generated UI file from linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 poetry run cruft check
 poetry run mypy --ignore-missing-imports streamdeck_ui/
 poetry run isort --check --diff streamdeck_ui/ tests/ --skip ui_main.py
-poetry run black --check streamdeck_ui/ tests/
+poetry run black --check streamdeck_ui/ tests/ --exclude ui_main.py
 poetry run flake8 streamdeck_ui/ tests/ --ignore F403,F401,W503 --exclude ui_main.py
 poetry run safety check -i 39462
 poetry run bandit -r streamdeck_ui/
