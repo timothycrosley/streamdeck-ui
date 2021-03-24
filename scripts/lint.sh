33426a8393a574fcf467baea8496f2d3732140af
Pieter Venter <dodgyrabbit@hotmail.com>
2021-03-24 13:30:03 -0400

    Limit OS options
    Trying to get actions to work, no need to waste action minutes
    Excude auto generated file from linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 poetry run cruft check
 poetry run mypy --ignore-missing-imports streamdeck_ui/
-poetry run isort --check --diff streamdeck_ui/ tests/
+poetry run isort --check --diff streamdeck_ui/ tests/ --skip ui_main.py
 poetry run black --check streamdeck_ui/ tests/
 poetry run flake8 streamdeck_ui/ tests/ --ignore F403,F401,W503 --exclude ui_main.py
 poetry run safety check -i 39462
