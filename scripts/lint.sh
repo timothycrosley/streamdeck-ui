42415cc41e7a112df364439b4207ef46f6e69efd
Pieter Venter <dodgyrabbit@hotmail.com>
2022-02-08 00:02:39 -0500

    Line length of 200

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 poetry run cruft check
 poetry run mypy --ignore-missing-imports streamdeck_ui/
 poetry run isort --check --diff streamdeck_ui/ tests/ --skip ui_main.py --skip resources_rc.py --skip ui_settings.py
-poetry run black --check streamdeck_ui/ tests/ --exclude 'ui_main.py|resources_rc.py|ui_settings.py'
+poetry run black --check --diff streamdeck_ui/ tests/ --exclude 'ui_main.py|resources_rc.py|ui_settings.py' --line-length 200
 poetry run flake8 streamdeck_ui/ tests/ --ignore F403,F401,W503 --exclude ui_main.py,resources_rc.py,ui_settings.py
 poetry run safety check -i 39462 -i 40291 -i 44715
 poetry run bandit -r streamdeck_ui/
