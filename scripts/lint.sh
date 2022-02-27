97e5718865a73eb6fac3ecdee8f3b70fa41f25b8
Pieter Venter <dodgyrabbit@hotmail.com>
2022-02-26 20:00:13 -0500

    Ignore compiled files

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy --ignore-missing-imports streamdeck_ui/
+poetry run mypy --ignore-missing-imports streamdeck_ui/ --exclude 'ui_main.py|resources_rc.py|ui_settings.py'
 poetry run isort --check --diff streamdeck_ui/ tests/ --skip ui_main.py --skip resources_rc.py --skip ui_settings.py --line-length 200
 poetry run black --check --diff streamdeck_ui/ tests/ --exclude 'ui_main.py|resources_rc.py|ui_settings.py' --line-length 200
 poetry run flake8 streamdeck_ui/ tests/ --ignore F403,F401,W503 --exclude ui_main.py,resources_rc.py,ui_settings.py
