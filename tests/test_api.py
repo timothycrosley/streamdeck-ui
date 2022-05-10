e8cc045af164da69635e936fe61153f0fe5949d7
Pieter Venter <dodgyrabbit@hotmail.com>
2022-05-09 22:17:52 -0400

    Fix tests
    WIP but bring tests back up to running state

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,20 @@
+import time
+
 from hypothesis_auto import auto_pytest_magic
 
 from streamdeck_ui import api
 
-auto_pytest_magic(api.set_button_command)
-auto_pytest_magic(api.get_button_command)
-auto_pytest_magic(api.set_button_switch_page)
-auto_pytest_magic(api.get_button_switch_page)
-auto_pytest_magic(api.set_button_keys)
-auto_pytest_magic(api.get_button_keys)
-auto_pytest_magic(api.set_button_write)
-auto_pytest_magic(api.get_button_write)
-auto_pytest_magic(api.set_brightness, auto_allow_exceptions_=(KeyError,))
-auto_pytest_magic(api.get_brightness)
-auto_pytest_magic(api.change_brightness, auto_allow_exceptions_=(KeyError,))
-auto_pytest_magic(api.set_page)
-auto_pytest_magic(api.get_page)
-auto_pytest_magic(api.render)
+server = api.StreamDeckServer()
+
+auto_pytest_magic(server.set_button_command)
+auto_pytest_magic(server.get_button_command)
+auto_pytest_magic(server.set_button_switch_page)
+auto_pytest_magic(server.get_button_switch_page)
+auto_pytest_magic(server.set_button_keys)
+auto_pytest_magic(server.get_button_keys)
+auto_pytest_magic(server.set_button_write)
+auto_pytest_magic(server.get_button_write)
+auto_pytest_magic(server.set_brightness, auto_allow_exceptions_=(KeyError,))
+auto_pytest_magic(server.get_brightness)
+auto_pytest_magic(server.change_brightness, auto_allow_exceptions_=(KeyError,))
+auto_pytest_magic(server.get_page)
--- tests/test_stream_deck_monitor.py
+++ tests/test_stream_deck_monitor.py
@@ -1,3 +1,4 @@
+import threading
 from time import sleep
 from unittest import TestCase
 from unittest.mock import patch
@@ -6,18 +7,9 @@ from streamdeck_ui.stream_deck_monitor import StreamDeckMonitor
 
 
 def test_start_stop():
-    monitor = StreamDeckMonitor()
+    lock: threading.Lock = threading.Lock()
+    monitor = StreamDeckMonitor(lock, lambda serial, deck: None, lambda serial: None)
     monitor.start()
     # FIXME: Need to find a better way to find "evidence" that it worked
     sleep(1)
     monitor.stop()
-
-
-def test_new_deck():
-    # TODO: How to mock this stuff? We need a function that can return
-    # a list and change how it behaves on subsequent calls.
-    monitor = StreamDeckMonitor()
-    monitor.start()
-    # FIXME: Need to find a better way to find "evidence" that it worked
-    while True:
-        sleep(1)
