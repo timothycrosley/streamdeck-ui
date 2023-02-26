5b828e6eebb594362f7baadc37ec37395fda9e93
Pieter Venter <dodgyrabbit@hotmail.com>
2023-02-26 00:21:00 -0500

    Temporarily remove to see what happens

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-from unittest.mock import MagicMock
+#from unittest.mock import MagicMock
 
-from streamdeck_ui import api
+#from streamdeck_ui import api
 
-api.DeviceManager = MagicMock()
-api.StreamDeck = MagicMock()
-api.ImageHelpers = MagicMock()
-api.export_config = MagicMock()
+#api.DeviceManager = MagicMock()
+#api.StreamDeck = MagicMock()
+#api.ImageHelpers = MagicMock()
+#api.export_config = MagicMock()
