18b99eb89f747c1a43668053c715ea7f1f115f6f
Pieter Venter <dodgyrabbit@hotmail.com>
2023-02-26 00:38:08 -0500

    Try again

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-#from unittest.mock import MagicMock
-
-#from streamdeck_ui import api
-
-#api.DeviceManager = MagicMock()
-#api.StreamDeck = MagicMock()
-#api.ImageHelpers = MagicMock()
-#api.export_config = MagicMock()
