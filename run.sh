adfc1c2746cf6c8153235538e77374d92156c64e
Moreno Razzoli <morrolinux@gmail.com>
2024-01-15 10:22:47 +0100

    workaround to startup initialization bug

--- run.sh
+++ run.sh
@@ -3,4 +3,8 @@
 source /opt/anaconda/bin/activate root
 conda activate streamdeck
 cd ~/streamdeck-ui 
+
+python3 -m poetry run streamdeck &
+sleep 3
+kill $(pgrep -f "streamdeck/bin/python")
 python3 -m poetry run streamdeck
